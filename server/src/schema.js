0e5b163c61d21a2420b7dc9a8b15db6ad3e7c9c9
John William Sheffield Jr <john@sheffinet.org>
2022-12-04 15:17:47 -0500

    Update schema.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -19,6 +19,12 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -29,6 +35,15 @@ const typeDefs = gql`
     "Author's profile picture"
     photo: String
   }
+  
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+}
 `;
 
 module.exports = typeDefs;
