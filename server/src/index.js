780cf6c8da1c8e89a061c65504b97bcbec42b6f1
John William Sheffield Jr <john@sheffinet.org>
2022-12-04 17:09:27 -0500

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-
 const TrackAPI = require('./datasources/track-api');
 
 const server = new ApolloServer({
