d16f1c3266bd38b52a25fe3b273ab275d5fbc4e0
John William Sheffield Jr <john@sheffinet.org>
2022-12-08 02:23:09 +0000

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://johnsheffinet-obscure-succotash-wgpr6xg495h565q-4000.preview.app.github.dev/',
   cache: new InMemoryCache(),
 });
 
