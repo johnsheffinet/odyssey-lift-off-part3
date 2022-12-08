2d3a8232f4c0608f2f10252a2d7b045c0600d9b1
John William Sheffield Jr <john@sheffinet.org>
2022-12-07 20:56:36 -0500

    Update track.js

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -6,4 +6,28 @@ const Track = ({trackId}) => {
   return <Layout></Layout>;
 };
 
+export const GET_TRACK = gql`
+    // our query goes here
+    query GetTrack($trackId: ID!) {
+  track(id: $trackId) {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+    numberOfViews
+    modules {
+      id
+      title
+      length
+    }
+    description
+  }
+}
+`;
 export default Track;
